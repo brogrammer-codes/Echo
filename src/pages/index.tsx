971f778289bda36ed6de1ef1545a5e95c5824c0f
ralph-p <mr.pereira91@gmail.com>
2023-06-21 10:23:44 -0400

    added post example on front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import { Post } from "~/components/Post";
 import { api } from "~/utils/api";
+
+
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "Echo" });
@@ -7,6 +10,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+      <Post />
     </div>
   );
 }
