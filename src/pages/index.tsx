3f7a0910e0ac48586f9c98f178fcd9b58e077177
ralph-p <mr.pereira91@gmail.com>
2023-06-26 14:39:15 -0400

    added changes to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,37 @@
 import { useUser } from "@clerk/nextjs";
 import { LoadingPage } from "~/components/loading";
 import { Post } from "~/components/Post";
-import { api } from "~/utils/api";
+import { api, RouterOutputs } from "~/utils/api";
 import { CreatePostWizard } from "~/components/createPostWizard";
+import { useEffect, useState } from "react";
 
+
+const SortBar = () => {
+
+}
+type PostWithUser = RouterOutputs["posts"]["getAll"][number]
 
 export default function Home() {
-  const { data, isLoading } = api.posts.getAll.useQuery()
+  // const { data, isLoading } = api.posts.getAll.useQuery()
+  const [order, setOrder,] = useState<string>('asc')
+  const { data, isLoading, refetch } = api.posts.getAll.useQuery({order})
+  const [posts, setPosts,] = useState<PostWithUser[]>([])
   const { user, } = useUser()
+  useEffect(() => {
+    refetch()
+  }, [order])
   if (isLoading) return <LoadingPage />
   if (!data) return <div>Could not load feed</div>
+  
   return (
     <div className="flex flex-row w-full">
       <div className="flex flex-col w-full md:w-2/3 p-2">
         <div className="block md:hidden">
+          <div>
 
+          <button onClick={() => setOrder('asc')}>Asc</button>
+          <button onClick={() => setOrder('desc')}>Dsc</button>
+          </div>
           <CreatePostWizard />
         </div>
         {
