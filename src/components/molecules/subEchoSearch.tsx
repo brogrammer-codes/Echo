ecee93917868bcf7b3846da6b182da4f0fae834b
ralph-p <mr.pereira91@gmail.com>
2023-07-07 13:33:33 -0400

    added key prop

--- src/components/molecules/subEchoSearch.tsx
+++ src/components/molecules/subEchoSearch.tsx
@@ -19,12 +19,12 @@ export const SubEchoSearch = ({selectEcho}: Props) => {
   }
   return (
     <div>
-      <Input inputRef={searchRef} onBlur={() => searchRef.current && searchRef.current.value.length &&  setSearchParam(searchRef.current.value)} onKeyDown={onSearchChange} placeholder="Start typing and hit enter or click away to search for sub echos"/>
+      <Input inputRef={searchRef} onBlur={() => searchRef.current &&  setSearchParam(searchRef.current.value)} onKeyDown={onSearchChange} placeholder="Start typing and hit enter or click away to search for sub echos"/>
       {/* <input ref={searchRef} onBlur={() => searchRef.current && setSearchParam(searchRef.current.value)}/> */}
       <div className='flex flex-row space-x-2 p-2'>
 
         {
-          !isLoading && data ? data.map((sub) => <button className='px-2 rounded bg-slate-600' onClick={() => selectEcho(sub.title)}>{sub.title}</button>) : null
+          !isLoading && data ? data.map((sub) => <button key={`echo-${sub.title}`} className='px-2 rounded bg-slate-600' onClick={() => selectEcho(sub.title)}>{sub.title}</button>) : null
         }
       </div>
     </div>
